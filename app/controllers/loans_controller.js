db4c13501e7b230d1ac224beef3e50f26c80a45a
Adrian Lang <mail@adrianlang.de>
2013-06-25 12:50:32 +0200

    Use in-words library

--- app/controllers/loans_controller.js
+++ app/controllers/loans_controller.js
@@ -156,53 +156,7 @@ action(function put_loan_state() {
     });
 });
 
-function inWords(val, joinAfter) {
-  function join(a, joiner, b) {
-    var ret = a;
-    if (a && b) {
-      ret += joiner;
-    }
-    ret += b;
-    return ret;
-  }
-
-  if (val < 13) {
-    return {
-      0: '',
-      1: joinAfter ? 'ein' : 'eins',
-      2: 'zwei',
-      3: 'drei',
-      4: 'vier',
-      5: 'fünf',
-      6: 'sechs',
-      7: 'sieben',
-      8: 'acht',
-      9: 'neun',
-      10: 'zehn',
-      11: 'elf',
-      12: 'zwölf'
-    }[val];
-  } else if (val < 100) {
-    var prefix = inWords(val % 10);
-    return join(prefix, val > 20 ? 'und' : '', {
-      1: 'zehn',
-      2: 'zwanzig',
-      3: 'dreißig',
-      4: 'vierzig',
-      5: 'fünfzig',
-      6: 'sechzig',
-      7: 'siebzig',
-      8: 'achtzig',
-      9: 'neunzig'
-    }[Math.floor(val / 10)]);
-  } else if (val < 1000) {
-    return join(inWords(Math.floor(val / 100), true) + 'hundert', 'und', inWords(val % 100));
-  } else if (val < 1000000) {
-    return join(inWords(Math.floor(val / 1000), true) + 'tausend', 'und', inWords(val % 1000));
-  } else {
-    throw new Error('too big');
-  }
-}
+var inWords = require('in-words').de;
 
 action(function contract() {
   var render = require('node-pdf').render;
