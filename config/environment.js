35de42e17b3782a78e6cbfcac890d66586f6e980
Adrian Heine <mail@adrianheine.de>
2015-02-24 10:59:44 +0100

    Further removing unused stuff

--- config/environment.js
+++ config/environment.js
@@ -53,10 +53,6 @@ module.exports = function (compound) {
           });
         }
         app.use(express.static(app.root + '/public', { maxAge: 86400000 }));
-        app.set('jsDirectory', '/javascripts/');
-        app.set('cssDirectory', '/stylesheets/');
-        app.set('cssEngine', 'stylus');
-        app.set('autoupdate', 'on');
 
         app.use(express.bodyParser());
         app.use(express.cookieParser('secret'));
